8a3e4baa12339edb2ba382e4916970afaf99a341
coffmanjrp <coffmanjrp@gmail.com>
2022-05-15 14:11:32 -0700

    Create a submit endpoint

--- app.py
+++ app.py
@@ -8,6 +8,16 @@ def index():
     return render_template('index.html')
 
 
+@app.route('/submit', methods=['POST'])
+def submit():
+    if request.method == 'POST':
+        customer = request.form['customer']
+        dealer = request.form['dealer']
+        rating = request.form['rating']
+        comments = request.form['comments']
+        print(customer, dealer, rating, comments)
+
+
 if __name__ == '__main__':
     app.debug = True
     app.run()
